730eafa509aee05bcfaf7eb73c719f87c1b69c32
Mariam Padalka <mariam.padalka@gmail.com>
2026-01-14 21:41:12 -0500

    module ten homework

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,41 @@
+// Описаний у документації
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+
+console.log(form.elements);
+console.log(form.elements.delay.value);
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  const delay = Number(form.elements.delay.value);
+  const state = form.elements.state.value;
+
+  createPromise(delay, state)
+    .then(delay => {
+      iziToast.success({
+        message: `✅ Fulfilled promise in ${delay}ms`,
+        position: 'topRight',
+      });
+    })
+    .catch(delay => {
+      iziToast.error({
+        message: `❌ Rejected promise in ${delay}ms`,
+        position: 'topRight',
+      });
+    });
+});
+
+function createPromise(delay, state) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+}
